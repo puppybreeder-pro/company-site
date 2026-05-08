3cf22d7d06a291c86a0e17e367839e31d45d251d
Steven Olsen <sr.js.programmer@gmail.com>
2026-05-08 10:19:43 -0600

    update email, calendly api route

--- frontend-astro/src/constants/index.js
+++ frontend-astro/src/constants/index.js
@@ -1,12 +1,12 @@
-export const BRAND_NAME = 'PuppyBreeder';
-export const BRAND_SUFFIX = '.PRO';
-export const BRAND_EMAIL = 'steven@puppybreeder.pro';
-export const REPORT_EMAIL = 'report@puppybreeder.pro';
+export const BRAND_NAME = "PuppyBreeder";
+export const BRAND_SUFFIX = ".PRO";
+export const BRAND_EMAIL = "steven+hello@puppybreeder.pro";
+export const REPORT_EMAIL = "report@puppybreeder.pro";
 
-export const CALENDLY_URL = 'https://api.puppybreedr.pro/book-a-free-call';
+export const CALENDLY_URL = "https://puppybreeder.pro/book-a-free-call";
 
 export const TRUST_SIGNALS = {
-  launchTime: '72hrs',
-  breedDesigns: '28+',
-  hosting: '100%',
+  launchTime: "72hrs",
+  breedDesigns: "28+",
+  hosting: "100%",
 };
