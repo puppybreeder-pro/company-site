6cc1099b7858267a7e81621b73c71b1b0f604c9e
Steven Olsen <sr.js.programmer@gmail.com>
2026-05-11 18:42:02 -0600

    add gtag support with env var via partytown

--- frontend-astro/astro.config.mjs
+++ frontend-astro/astro.config.mjs
@@ -2,10 +2,20 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import icon from 'astro-icon';
 import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   site: 'https://www.puppybreeder.pro',
-  integrations: [react(), icon(), sitemap()],
+  integrations: [
+    react(),
+    icon(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
   vite: {
     resolve: {
       alias: { '@': new URL('./src', import.meta.url).pathname },
