f73a5d15aa52d092b9b4b5164fb03923f6f3863f
Steven Olsen <sr.js.programmer@gmail.com>
2026-05-12 11:35:35 -0600

    npm audit fixes, minor code cleanup

--- frontend-astro/astro.config.mjs
+++ frontend-astro/astro.config.mjs
@@ -1,8 +1,11 @@
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import icon from 'astro-icon';
 import sitemap from '@astrojs/sitemap';
 import partytown from '@astrojs/partytown';
+
+const srcDir = fileURLToPath(new URL('./src', import.meta.url));
 
 export default defineConfig({
   site: 'https://www.puppybreeder.pro',
@@ -18,7 +21,7 @@ export default defineConfig({
   ],
   vite: {
     resolve: {
-      alias: { '@': new URL('./src', import.meta.url).pathname },
+      alias: { '@': srcDir },
     },
   },
 });
